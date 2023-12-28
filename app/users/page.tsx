29cbd837ff3ed17d6a1eb1e6c65fe1e6d7c915aa
z0zero <briansangapta@gmail.com>
2023-12-28 17:41:25 +0700

    Static Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,14 +6,13 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch(
-    'https://jsonplaceholder.typicode.com/users',
-    { next: { revalidate: 10}});
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await res.json();
 
   return (
     <div>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user: any) => (
           <li key={user.id}>

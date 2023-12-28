87549bf29bc60d285a62835760e0a4934a9e1ce4
z0zero <briansangapta@gmail.com>
2023-12-28 22:57:31 +0700

    Accessing Query String Parameters

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { sort } from 'fast-sort';
 
 interface User {
     id: number;
@@ -6,22 +8,38 @@ interface User {
     email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+    sortOrder: string;
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const users = await res.json();
+    const users: User[] = await res.json();
+
+    const sortedUsers = sort(users).asc(
+        sortOrder === "email"
+            ? (user) => user.email
+            : (user) => user.name
+    );
 
     return (
         <table className='table table-bordered'>
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Email</th>
+                    <th>
+                        <Link href="/users?sortOrder=name">Name</Link>
+                    </th>
+                    <th>
+                        <Link href="/users?sortOrder=email">Email</Link>
+                    </th>
                 </tr>
             </thead>
             <tbody>
-                {users.map((user: User) => <tr key={user.id}>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td></tr>)}
+                {sortedUsers.map((user: User) =>
+                    <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                    </tr>)}
             </tbody>
         </table>
     )

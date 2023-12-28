7cacbf11742efd9397f70c2cf2272650fc2a1b63
z0zero <briansangapta@gmail.com>
2023-12-28 20:55:11 +0700

    Static Routes

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,32 +1,14 @@
 import React from 'react'
+import UserTable from './UserTable'
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users',
-  { cache: 'no-store'});
-  const users = await res.json();
 
   return (
     <div>
       <h1>Users</h1>
-      <table className='table table-bordered'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-            {users.map((user: User) => <tr key={user.id}>
-            <td>{user.name}</td>
-            <td>{user.email}</td></tr>)}
-        </tbody>
-      </table>
+      <UserTable />
     </div>
   )
 }

58144a23d2609242a93bcb6a515ac61e56e9185d
z0zero <briansangapta@gmail.com>
2023-12-28 15:55:09 +0700

    Client and Server Component

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+export const AddToCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('Click')}>Add to Cart</button>
+    </div>
+  )
+}
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AddToCart } from './AddToCart';
+
+export const ProductCard = () => {
+  return (
+    <div>
+        <AddToCart/>
+    </div>
+  )
+}

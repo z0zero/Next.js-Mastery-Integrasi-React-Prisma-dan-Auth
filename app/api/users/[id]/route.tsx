dd1bcf6d36d262597adbc8e375f4d253cf010ab4
z0zero <briansangapta@gmail.com>
2023-12-30 22:17:10 +0700

    Deleting an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -21,3 +21,12 @@ export async function PUT(
 
   return NextResponse.json({ id: 1, name: body.name });
 }
+
+export async function DELETE(
+  request: NextRequest, 
+  { params }: { params: { id: number } }){
+  if (params.id > 10)
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+  return NextResponse.json({  });
+}

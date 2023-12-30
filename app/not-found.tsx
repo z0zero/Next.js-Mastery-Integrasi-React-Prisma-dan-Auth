f5d32a52773c355db1a2fb714f874888645ab1b2
z0zero <briansangapta@gmail.com>
2023-12-30 21:41:12 +0700

    Handling Enexpected Errors

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
         <p className="text-base font-semibold text-black">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
@@ -19,7 +19,7 @@ const NotFoundPage = () => {
           </a>
         </div>
       </div>
-    </main>
+    </div>
   )
 }
 

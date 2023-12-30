403f196d639002be98ba29406bdea1ff1337cb54
z0zero <briansangapta@gmail.com>
2023-12-31 00:30:07 +0700

    Exercise- Storing Products in DB

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";  
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: 'Milk', price: 1.99 },
-    { id: 2, name: 'Bread', price: 2.99 },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -13,5 +12,13 @@ export async function POST(request: NextRequest) {
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 }); // Bad Request
-  return NextResponse.json({ id: 10, name: body.name, price: body.price }, { status: 201 }); // Created
+
+  const newProduct = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price
+    }
+  });
+
+  return NextResponse.json(newProduct, { status: 201 }); // Created
 }

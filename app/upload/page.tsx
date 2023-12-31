4621c45abd7d4526011064ea5ca0ba50935cf63a
z0zero <briansangapta@gmail.com>
2023-12-31 15:13:16 +0700

    Customizing the Upload Widget

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -14,6 +14,12 @@ const UploadPage = () => {
         <CldImage src={publicId} width={270} height={180} alt='A image' />}
       <CldUploadWidget
         uploadPreset='cqqrzuhb'
+        options={{
+          sources: ['local'],
+          multiple: false,
+          maxFiles: 5,
+          styles: {},
+        }}
         onUpload={(result, widget) => {
           if (result.event !== 'success') return;
           const info = result.info as CloudinaryResult;

d6466a78c46f2bb2d0da2d4624ca7a000aaab97c
z0zero <briansangapta@gmail.com>
2024-01-01 14:15:16 +0700

    Setting up React Email and Fix compile issue with @react-email/render
    Update @react-email/render to 0.0.9-canary.2 to resolve import error of renderToReadableStream not being exported from react-dom/server. This update aligns with the latest API changes in React 18 for server-side rendering.

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,41 @@
+import React, { CSSProperties } from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Tailwind,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+import { renderAsync } from '@react-email/render';
+
+const WelcomeTemplate = ({ name }: { name: string }) => {
+  return (
+    <Html>
+      <Preview>Welcome aboard!</Preview>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">
+              Hello {name}
+            </Text>
+            <Link href="https://myanimelist.net/anime/38816/Hello_World">
+            Hello World
+            </Link>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+const body: CSSProperties = {
+  background: "#fff",
+};
+
+const heading: CSSProperties = {
+  fontSize: "32px",
+};
+
+export default WelcomeTemplate;

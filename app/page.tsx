b7fd93d7082acbaef9b946b67fce37c0c5a9f922
z0zero <briansangapta@gmail.com>
2024-01-05 20:58:26 +0700

    Using Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,7 @@ import coffe from '@/public/images/coffee.jpg'
 export default async function Home() {
   return (
     <main className='relative h-screen'>
-      <Image src='https://bit.ly/react-cover' 
-        alt='Coffee' 
-        fill
-        className='object-cover'
-        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-        quality={100}
-        priority
-        />
+      <h1>Hello World</h1>
     </main>
   )
 }
